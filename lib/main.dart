3196aec1d5ef5465134f0020ff8caf43da736090
paisible7 <22km127@esisalama.org>
2025-04-02 22:04:43 +0200

    COMMIT_02_AVRIL_2025 ==> dependance et initialisation de supabase

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,13 @@
 import 'package:flutter/material.dart';
 import 'presentation/screens/HomeScreen.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: "https://wfzkwqicqeiegoudcdro.supabase.co" ,
+    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indmemt3cWljcWVpZWdvdWRjZHJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1ODYyMzYsImV4cCI6MjA1OTE2MjIzNn0.C9ySRTy0eT4yEQNtFgqrnZRWFNqCARQzlFYLHwWcG6w" ,
+  );
   runApp(const MyJersey());
 }
 
