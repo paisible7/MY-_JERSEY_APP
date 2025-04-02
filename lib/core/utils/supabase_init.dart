a295711446251cd7e3fd96822642f0840e36406f
paisible7 <22km127@esisalama.org>
2025-04-02 22:27:19 +0200

    COMMIT_02_AVRIL_2025 ==> ajout de login et signup et initalisation des tables supabase

---
+++ lib/core/utils/supabase_init.dart
@@ -0,0 +1,11 @@
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+class SupabaseConfig {
+  static Future<void> initialize() async {
+    await Supabase.initialize(
+      url: "https://wfzkwqicqeiegoudcdro.supabase.co" ,
+      anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indmemt3cWljcWVpZWdvdWRjZHJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1ODYyMzYsImV4cCI6MjA1OTE2MjIzNn0.C9ySRTy0eT4yEQNtFgqrnZRWFNqCARQzlFYLHwWcG6w" ,
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:myjersey/core/utils/supabase_init.dart';
+import 'package:myjersey/presentation/screens/Login_Screen.dart';
+import 'package:myjersey/presentation/screens/Signup_Screen.dart';
 import 'presentation/screens/HomeScreen.dart';
-import 'package:supabase_flutter/supabase_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Supabase.initialize(
-    url: "https://wfzkwqicqeiegoudcdro.supabase.co" ,
-    anonKey: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Indmemt3cWljcWVpZWdvdWRjZHJvIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM1ODYyMzYsImV4cCI6MjA1OTE2MjIzNn0.C9ySRTy0eT4yEQNtFgqrnZRWFNqCARQzlFYLHwWcG6w" ,
-  );
+  await SupabaseConfig.initialize();
   runApp(const MyJersey());
 }
 
@@ -17,7 +16,12 @@ class MyJersey extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Homescreen(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => LoginScreen(),
+        '/signup': (context) => SignupScreen(),
+        '/home': (context) => Homescreen(),
+      },
     );
   }
 }
